3833d2294aeae3412844f5455f9845bd695b170b
aertuss <maciej.tyrcha@o2.pl>
2019-01-18 16:15:59 +0100

    scrolling nav widh jQuery

--- main.js
+++ main.js
@@ -9,3 +9,66 @@ const openCloseNav = () => {
 }
 
 hamburgerMenu.addEventListener("click", openCloseNav);
+
+//Nav scrollowanie do konkretnych sekcji
+
+const windowWidth = $(window).width();
+
+$(".navHome").on("click", function (event) {
+  event.preventDefault();
+  $("body, html").animate({
+    scrollTop: $(".mainPage").offset().top - 70
+  }, 1000);
+});
+
+$(".navAboutMe").on("click", function (event) {
+  event.preventDefault();
+  if (windowWidth >= 900) {
+    $("body, html").animate({
+      scrollTop: $(".aboutMe").offset().top
+    }, 1000);
+  } else {
+    $("body, html").animate({
+      scrollTop: $(".aboutMe").offset().top - 70
+    }, 1000);
+  }
+});
+
+$(".navPortfolio").on("click", function (event) {
+  event.preventDefault();
+  if (windowWidth >= 900) {
+    $("body, html").animate({
+      scrollTop: $(".projects").offset().top + 100
+    }, 1000);
+  } else {
+    $("body, html").animate({
+      scrollTop: $(".projects").offset().top - 30
+    }, 1000);
+  }
+});
+
+$(".navContact").on("click", function (event) {
+  event.preventDefault();
+  if (windowWidth >= 900) {
+    $("body, html").animate({
+      scrollTop: $("footer").offset().top
+    }, 1000);
+  } else {
+    $("body, html").animate({
+      scrollTop: $("footer").offset().top - 70
+    }, 1000);
+  }
+});
+
+$(".mainPage div").on("click", function (event) {
+  event.preventDefault();
+  if (windowWidth >= 900) {
+    $("body, html").animate({
+      scrollTop: $(".projects").offset().top + 100
+    }, 1000);
+  } else {
+    $("body, html").animate({
+      scrollTop: $(".projects").offset().top - 30
+    }, 1000);
+  }
+});
